132980adcccf79a631aae72b6160a2644a26a182
Antonio Tapiador del Dujo <atd@singularities.org>
2022-01-31 23:39:09 +0100

    Infinite scroll

--- app/javascript/packs/components/Songs/index.tsx
+++ app/javascript/packs/components/Songs/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react'
 import { observer } from 'mobx-react'
-import { VStack } from '@chakra-ui/react'
+import { Spinner, VStack } from '@chakra-ui/react'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 import { useCollections } from '../Collections'
 
 import Song from './Song'
+
+const InitialSongCount = 3
 
 const Songs = () => {
   const { songs } = useCollections()
@@ -13,12 +16,39 @@ const Songs = () => {
     songs
       .toArray()
       .sort((a, b) => (a.get('created_at') > b.get('created_at') ? -1 : 1))
+  const [hasMore, setHasMore] = React.useState(true)
+  const [displayedSongsCount, setDisplayedSongsCount] = React.useState(InitialSongCount)
+  const [displayedSongs, setDisplayedSongs] = React.useState([])
+
+  const fetchMoreSongs = () => {
+    if (sortedSongs.length <= displayedSongsCount) {
+      setHasMore(false)
+      return
+    }
+
+    displayedSongs.push(sortedSongs[displayedSongsCount])
+    setDisplayedSongs(displayedSongs)
+    setDisplayedSongsCount(displayedSongsCount + 1)
+  }
+
+  if (!displayedSongs.length && sortedSongs.length) {
+    setDisplayedSongs(sortedSongs.slice(0, displayedSongsCount))
+  } else if (displayedSongs.length && displayedSongs[0] !== sortedSongs[0]) {
+    setDisplayedSongs(sortedSongs.slice(0, displayedSongsCount))
+    setDisplayedSongsCount(InitialSongCount)
+  }
 
   return (
     <VStack spacing={8} mt='3' align='stretch'>
-      {sortedSongs.map(song => (
-        <Song key={song.id} song={song} />
-      ))}
+      <InfiniteScroll
+        dataLength={displayedSongs.length}
+        next={fetchMoreSongs}
+        hasMore={hasMore}
+        loader={<Spinner />} >
+        {displayedSongs.map(song => (
+          <Song key={song.id} song={song} />
+        ))}
+      </InfiniteScroll>
     </VStack>
   )
 }
