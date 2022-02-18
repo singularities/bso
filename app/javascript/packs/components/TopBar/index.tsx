57e27390c6b4eac3bb25b48140c5c3c85696f4a0
Antonio Tapiador del Dujo <atd@singularities.org>
2022-02-19 00:49:37 +0100

    Add whatsapp button

--- app/javascript/packs/components/TopBar/index.tsx
+++ app/javascript/packs/components/TopBar/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
-import { Container, Flex, Spacer } from '@chakra-ui/react'
+import { Container, Flex, Icon, Link, Spacer } from '@chakra-ui/react'
+import { FaWhatsapp } from 'react-icons/fa'
 
 import Logo from './Logo'
 import AuthStatus from '../Auth/Status'
@@ -7,9 +8,12 @@ import AuthStatus from '../Auth/Status'
 const TopBar = () => {
   return (
     <Container mt='2'>
-      <Flex flexDirection='row'>
+      <Flex flexDirection='row' alignItems='center'>
         <Logo/>
         <Spacer />
+        <Link href='whatsapp://send?text=Nuestra%20banda%20sonora%20con%20Marta%20https://bso.singularities.org/'>
+          <Icon as={FaWhatsapp} w='8' h='8' mt='2'/>
+        </Link>
         <AuthStatus />
       </Flex>
 
