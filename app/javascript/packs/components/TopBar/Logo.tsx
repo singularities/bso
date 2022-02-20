f538ccdd4635b231c515869b06030b2352376ac9
Antonio Tapiador del Dujo <atd@singularities.org>
2022-02-20 21:45:03 +0100

    Use webpack for images and logos

--- app/javascript/packs/components/TopBar/Logo.tsx
+++ app/javascript/packs/components/TopBar/Logo.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import {
   Image
 } from '@chakra-ui/react'
-import logo from '../../assets/images/logo.svg'
+import logo from '../../../images/logo.svg'
 
 const Logo = () => {
   return <Image boxSize='32px' src={logo} />
--- app/javascript/packs/pages/Front/Logo.tsx
+++ app/javascript/packs/pages/Front/Logo.tsx
@@ -6,7 +6,7 @@ import {
   forwardRef,
   usePrefersReducedMotion,
 } from '@chakra-ui/react'
-import logo from '../../assets/images/logo.svg'
+import logo from '../../../images/logo.svg'
 
 const spin = keyframes`
   from { transform: rotate(0deg); }
