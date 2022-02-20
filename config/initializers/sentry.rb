1d01461691d960ef303e7ab487867eaaec793c9b
Antonio Tapiador del Dujo <atd@singularities.org>
2022-02-20 22:57:20 +0100

    Sentry in backend

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,13 @@
+Sentry.init do |config|
+  config.dsn = 'https://12589f9bfda04178b2db9c539b323ef7@o1148925.ingest.sentry.io/6220458'
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Set tracesSampleRate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |_context|
+    true
+  end
+end
