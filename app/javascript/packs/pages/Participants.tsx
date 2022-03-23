df6aa7071e094a4b13de94029488cd1479bc8c41
Antonio Tapiador del Dujo <atd@singularities.org>
2022-03-23 22:49:43 +0100

    Participants message

--- app/javascript/packs/pages/Participants.tsx
+++ app/javascript/packs/pages/Participants.tsx
@@ -1,4 +1,6 @@
 import * as React from "react"
+import { Box, Flex, Icon } from "@chakra-ui/react"
+import { FaGift } from "react-icons/fa"
 
 import MembershipsCollection from '../stores/collections/Memberships'
 import Groups from '../components/Groups'
@@ -11,6 +13,21 @@ const ParticipantsPage = () => {
 
   return (
     <>
+      <Flex
+        borderColor='teal.200'
+        borderWidth={1}
+        backgroundColor='teal.50'
+        p={3}
+        borderRadius={5}
+        mb={3}
+      >
+        <Icon as={FaGift} h={8} w={8} color='teal.300' />
+        <Box ml={3} color='gray.600'>
+          Este regalo solo tiene sentido gracias a vuestra participación.
+          Sois quienes traéis los recuerdos y las emociones.
+          ¡Gracias por participar!
+        </Box>
+      </Flex>
       <Groups />
       <GrouplessUsers />
     </>
